36f4b6332c25ed02b9b4bb8545762a178ece6be8
mert.cotuk <mert.cotuk@inomera.com>
2022-01-02 19:59:57 +0300

    Initial commit

---
+++ src/test/java/com/bitpace/coincalculator/CoincalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitpace.coincalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoincalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
