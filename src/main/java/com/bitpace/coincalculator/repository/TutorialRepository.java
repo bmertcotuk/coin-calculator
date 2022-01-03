7c0defa54a9078d4cd684e765c2d8727a3d4dce6
mert.cotuk <mert.cotuk@inomera.com>
2022-01-04 01:52:20 +0300

    Initial commit
    * Initializes a full-stack project using
    the required tech stack

---
+++ src/main/java/com/bitpace/coincalculator/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.bitpace.coincalculator.repository;
+
+import com.bitpace.coincalculator.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+  List<Tutorial> findByPublished(boolean published);
+
+  List<Tutorial> findByTitleContaining(String title);
+}
