1f44ad4037e4aed8c77e45fc9f38c0ea99c9c258
mert.cotuk <mert.cotuk@inomera.com>
2022-01-09 03:45:35 +0300

    Finalize the project.

--- frontend/ui/src/components/ConvertCoin.vue
+++ frontend/ui/src/components/ConvertCoin.vue
@@ -14,6 +14,7 @@
           <input
               class="currency-input"
               type="number"
+              min='25' max='5000'
               name="input-one"
               id="input-one"
               v-model="amountOne"
@@ -77,7 +78,7 @@ export default {
       data: [],
       currencyOne: "USD",
       currencyTwo: "BTC",
-      amountOne: 1,
+      amountOne: 25,
       amountTwo: 0,
       countDown : 10,
       lastUpdateDate: "-"
